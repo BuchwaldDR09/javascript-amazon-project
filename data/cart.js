809f51a0e6fdca98b15943776013e8fac814dd47
BuchwaldDR09 <BuchwaldDR09@Gmail.com>
2025-10-13 15:10:11 -0500

    loading/saving to/from local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,15 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];;
+}
+
 
 export function AddToCart(productId, selectedQty) {
   let matchingItem;
@@ -23,6 +28,7 @@ export function AddToCart(productId, selectedQty) {
       quantity: selectedQty
     });
   }
+  SaveToStorage();
 }
 
 export function RemoveFromCart(productId) {
@@ -36,4 +42,11 @@ export function RemoveFromCart(productId) {
   })
   //3. Replace original cart
   cart = newCart;
+
+  SaveToStorage();
+}
+
+//save to localstorage
+function SaveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
